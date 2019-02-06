6e570f594941bf60a937377c8d9396f42531f694
hongdal <hongdal@clemson.edu>
2019-02-06 14:01:29 -0500

    add test interface to PatternGraph

--- module-dev/subgraph/PatternExtractor/PatternGraph.py
+++ module-dev/subgraph/PatternExtractor/PatternGraph.py
@@ -1,4 +1,5 @@
 import networkx as nx 
+from networkx.drawing import nx_pydot
 
 
 class PatternGraph:
@@ -11,18 +12,41 @@ class PatternGraph:
         name        :   a unique name of the graph, usually this is the name of the bash file. 
 
     '''
-    def __init__(self, nxgraph, name):
-        self.nxgraph = nxgraph
-        self.name = name
+    def __init__(self):
+        self.graph = None
+        self.fname = ""
+
+
+    def load_file(self, fname):
+        self.fname = fname
+        self.graph = nx.Graph(nx_pydot.read_dot(fname))
 
 
     def get_edges(self):
-        return self.nxgraph.edges
+        return self.graph.edges
+
 
     def get_nodes(self):
-        return self.nxgraph.nodes
+        return self.graph.nodes
 
-     
+    def get_nodes(self)
+
+
+
+fname = "../testcase/VirusShare_aa91a02e32e0aa714d0832f4ef86fba6.dot"
+
+def test():
+    pg = PatternGraph()
+    pg.load_file(fname)
+    edges = pg.get_edges()
+    nodes = pg.get_nodes()
+    print(edges)
+    print(nodes)
+
+if __name__ == "__main__":
+    test()
+
+
 
 
 
